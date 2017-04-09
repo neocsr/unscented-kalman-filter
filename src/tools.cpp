bfd23b267733c28e0c84baf8e5d0b7a363c3e107
Christian <neocsr@gmail.com>
2017-04-08 20:56:16 -0700

    Add rmse

--- src/tools.cpp
+++ src/tools.cpp
@@ -15,4 +15,23 @@ VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
   TODO:
     * Calculate the RMSE here.
   */
+  VectorXd rmse(4);
+  rmse << 0, 0, 0, 0;
+
+  if (estimations.size() != ground_truth.size() || estimations.size() == 0) {
+    throw std::invalid_argument("Estimations or ground truth data size error");
+  }
+
+  // Calculate squared residuals
+  for (int i = 0; i < estimations.size(); i++) {
+    VectorXd residual = estimations[i] - ground_truth[i];
+    residual = residual.array() * residual.array();
+    rmse += residual;
+  }
+
+  // Calculate the mean and squared root
+  rmse = rmse / estimations.size();
+  rmse = rmse.array().sqrt();
+
+  return rmse;
 }
